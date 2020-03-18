43b9e81a177f1a6544c38c8d3d0a69841a903b74
Felix Mayr <felixmayr99@gmail.com>
2020-03-18 19:30:30 +0100

    Finished Userlist, Removed JQuery, Added TwoFactor Delete

--- js/user.js
+++ js/user.js
@@ -1,46 +1,102 @@
-$(document).ready(function() {
+let userListBody;
+let form;
+let input;
+let feedDiv;
+let valid = false; // Form Valid State
+let delButtonPress = false; // Delete Button Confirmation Press State
 
-    var userListBody = $('.userList tbody');
+let users = []; // User Array
 
-    //@todo store and somehow update the current number of users
+document.addEventListener("DOMContentLoaded", () => {
+    userListBody = document.querySelector('.userList tbody');
+    form = document.querySelector('.needs-validation');
+    input = form.querySelector('input');
+    feedDiv = form.querySelector('.invalid-feedback');
 
 
-    $('.needs-validation').submit(function(event) {
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+        inputCheck(true);
+        addUser();
+    });
 
-        event.preventDefault();
-        event.stopPropagation();
+    input.addEventListener('focus', () => {
+        inputCheck(false);
+    });
 
-        if (this.checkValidity() === false) {
+    input.addEventListener('keyup', () => {
+        feedDiv.innerHTML = "Benutzername darf nicht leer sein!";
+        inputCheck(false);
+        form.classList.add('was-validated');
+    });
 
-            $(this).addClass('was-validated');
+    input.addEventListener('focusout', () => {
+        form.classList.remove('was-validated'); // Set Form unvalidated
+        input.setCustomValidity(""); // Set Input Element valid
+    });
+});
 
+const inputCheck = (submit) => {
+    let inputText = input.value;
+    if (submit) {
+        if (inputText.length === 0) {
+            feedDiv.innerHTML = "Benutzername darf nicht leer sein!"; // Set Error Div Text
+            form.classList.add('was-validated'); // Set Form validated
+            input.setCustomValidity("Username empty"); // Set Input Element invalid
+            valid = false;
+        }
+    } else {
+        if (checkUser(inputText) === false) {
+            feedDiv.innerHTML = "Benutzername \"" + inputText + "\" ist bereits vergeben!"; // Set Error Div Text
+            form.classList.add('was-validated'); // Set Form validated
+            input.setCustomValidity("Username taken"); // Set Input Element invalid
+            valid = false;
+        } else if (inputText.length > 0 && checkUser(input.value)) {
+            input.setCustomValidity(""); // Set Input Element valid
+            valid = true;
+        }
+    }
+};
+
+const checkUser = (user) => {
+    for (let i = 0; i < users.length; i++) {
+        if (users[i] === user) {
             return false;
         }
+    }
+    return true;
+};
 
-        //@todo
-        //1. get values
-        //2. create a new element
-        //3. somehow add them to userListBody
-        //4. update number of current users
-        //5. clear entries from the form
-        //6. maybe do something else... :-)
+const addUser = () => {
+    if (valid) {
+        users.push(input.value); // Add User to Array
+        generateTr(); // Rerender Table
+        input.value = ""; // Reset Input field
+        form.classList.remove('was-validated'); // Set Form unvalidated
+        valid = false;
+    }
+};
 
-        //your code follows here
+const deleteUser = (i) => {
+    if (delButtonPress) {
+        users.splice(i, 1); // Remove User from Array
+        generateTr(); // Rerender Table
+        delButtonPress = false;
+    } else {
+        document.querySelectorAll('.deleteTrigger')[i].querySelector('i').classList.replace('fa-trash', 'fa-check');
+        delButtonPress = true;
+    }
+};
 
-        return false;
-    });
-
-
-    $('.deleteTrigger').click(function() {
-        //@todo
-        //1. remove current user from dom
-        //2. update number of current users
-
-        //your code follows here
-
-
-    });
-
-    //maybe some code follows here
-
-});
+const generateTr = () => {
+    userListBody.innerHTML = ""; // Remove every entity from table
+    for (let i = 0; i < users.length; i++) {
+        let row = userListBody.insertRow(i);
+        let cell1 = row.insertCell(0);
+        let cell2 = row.insertCell(1);
+        let cell3 = row.insertCell(2);
+        cell1.innerHTML = i + 1;
+        cell2.innerHTML = users[i];
+        cell3.innerHTML = `<button type="button" class="btn btn-secondary btn-danger deleteTrigger" title="Löschen" onclick="deleteUser(${i})"><i class="fa fa-trash"></i></button>`;
+    }
+};
